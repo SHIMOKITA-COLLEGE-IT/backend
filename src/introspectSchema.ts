ed9cd2f382df90f683809a7279d534800f455dc7
千々岩 / Chijiiwa <shingo.pad@gmail.com>
2022-11-14 00:14:27 +0900

    fix

--- src/introspectSchema.ts
+++ src/introspectSchema.ts
@@ -6,14 +6,17 @@ import {
 import { AuthResolver } from './auth/auth.resolver';
 import { UsersResolver } from './users/users.resolver';
 import { printSchema } from 'graphql';
+import * as fs from 'fs';
 
 async function introspectSchema() {
-  const app = await NestFactory.create(GraphQLSchemaBuilderModule);
+  const app = await NestFactory.create(GraphQLSchemaBuilderModule, {
+    logger: false,
+  });
   await app.init();
 
   const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
   const schema = await gqlSchemaFactory.create([UsersResolver, AuthResolver]);
-  console.log(printSchema(schema));
+  fs.writeSync(1, printSchema(schema));
 }
 (async () => {
   await introspectSchema();
